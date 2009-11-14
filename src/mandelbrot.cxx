03e632e9725649d8ac00de41fd6c50c4bb3a200e
a_m0d <damien.schoof@gmail.com>
2009-11-14 22:18:32 +0800

    Added local variables to Generate; Now filling the screen with a white image ^_^

--- src/mandelbrot.cxx
+++ src/mandelbrot.cxx
@@ -12,8 +12,28 @@ Mandelbrot::~Mandelbrot () {
 }
 
 void Mandelbrot::Generate (int numIterations) {
+    float minReal = -2.0;
+    float maxReal = 1.0;
+    float minImg = -1.2;
+    float maxImg = minImg + (maxReal - minReal) *
+            myWindow.GetHeight()/myWindow.GetWidth();
+    float deltaReal = (maxReal - minReal) / (myWindow.GetWidth() - 1);
+    float deltaImg = (maxImg - minImg) / (myWindow.GetHeight() - 1);
 
-    return;
+    for (int y = 0; y < myWindow.GetHeight (); y++) {
+        float curImg = minImg + y * deltaImg;
+
+        for (int x = 0; x < myWindow.GetWidth (); x++) {
+            float curReal = minReal + x * deltaReal;
+
+            bool isInside = true;
+
+            if (isInside) {
+                myImage.SetPixel (x, y, sf::Color::White);
+            }
+
+        }
+    }
 }
 
 void Mandelbrot::Draw () {
